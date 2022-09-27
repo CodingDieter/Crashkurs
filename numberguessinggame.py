e288d0051a7dddc3a453e5f54c79d2e615ee8eda
Christian Achstetter <chacit00@hs-esslingen.de>
2022-09-27 18:14:18 +0200

    integer range check fixed

--- numberguessinggame.py
+++ numberguessinggame.py
@@ -7,10 +7,13 @@ print("guess a number between 0 - 10")
 while True:
     try:
         guessedValue = int(input())
+        if 0 <= guessedValue <= 10:
+            break
+        else:
+            print("input wasn`t in the given range you stupid piece of shit \n")
     except:
         print("input wasn`t a number you stupid piece of shit.. \n")
         print("try again: ")
-
 
 print("\n")
 
