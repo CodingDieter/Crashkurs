fcc32f2ccb8bd949a4bcb71cf8f36a8275f0bdf5
Christian Achstetter <chacit00@hs-esslingen.de>
2022-09-27 18:47:32 +0200

    main menue added

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-print("hallo dieter")
-print("scheisse funktionier doch endlich eyy")
+"""main brain   author: Christian"""
+
+from numberguessinggame import numberguessinggame
+
+while True:
+    print("Which game do you like to play?")
+    print()
+    print("1. guess the number")
+    print("2. not available")
+    print("3. not available")
+    print()
+    print("select option: ")
+
+    while True:
+        try:
+            selectedOption = int(input())
+            if 1 <= selectedOption <= 3:
+                break
+            else:
+                print("there is no game listed with that number you stupid piece of shit \n")
+        except:
+            print("input wasn`t even a number you stupid piece of shit.. \n")
+            print("try again: ")
+
+    if selectedOption == 1:
+        numberguessinggame()
--- numberguessinggame.py
+++ numberguessinggame.py
@@ -1,36 +1,33 @@
 from random import randrange
 
 """small game"""
-print("\n")
-print("guess a number between 0 - 10")
-
-while True:
-    try:
-        guessedValue = int(input())
-        if 0 <= guessedValue <= 10:
-            break
-        else:
-            print("input wasn`t in the given range you stupid piece of shit \n")
-    except:
-        print("input wasn`t a number you stupid piece of shit.. \n")
-        print("try again: ")
-
-print("\n")
-
-randomNumber = randrange(10)
-
-print("you chose: ", guessedValue)
-print("computer chose: ", randomNumber)
-print("\n")
-
-if randomNumber == guessedValue:
-    print("you won... congrats:)")
-else:
-    print("loooooser :(")
-
-print("\n")
 
 
+def numberguessinggame():
+    print()
+    print("guess a number between 0 - 10")
 
+    while True:
+        try:
+            guessedvalue = int(input())
+            if 0 <= guessedvalue <= 10:
+                break
+            else:
+                print("input wasn`t in the given range you stupid piece of shit \n")
 
+        except:
+            print("input wasn`t a number you stupid piece of shit.. \n")
+            print("try again: ")
 
+    print("\n")
+
+    randomnumber = randrange(10)
+
+    print("you chose: ", guessedvalue)
+    print("computer chose: ", randomnumber)
+    print("\n")
+
+    if randomnumber == guessedvalue:
+        print("you won... congrats:)")
+    else:
+        print("loooooser :(")
